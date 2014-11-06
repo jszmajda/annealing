45e022165d68f98b177aff11537f09b33120368c
Joshua Szmajda <josh@optoro.com>
2014-11-06 13:15:36 -0500

    refactored to one better main method

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -4,56 +4,50 @@ import qualified Park
 import qualified SVG
 import System.Random (getStdGen)
 
+annealTime :: Int
+annealTime = 100
+
+-- data PicnicEnv = PicnicEnv {
+--   people  :: [Park.Person]
+-- , park    :: [Polygon]
+-- , centers :: [Point]
+-- , sitting :: [Link]
+-- , walking :: [Link]
+-- }
 
 main :: IO ()
 main = do
-  putStrLn "------------"
   people_text <- readFile "people.txt"
+  park_data   <- readFile "park.svg"
 
-  let people :: [Park.Person]
-      people = read people_text
-
-  putStr "Number of people coming: "
-  print (length people)
-
-  let rainbow@[red,green,blue,yellow,purple,teal] = map SVG.colorize [(255,0,0),(0,255,0),(0,0,255),(255,255,0),(255,0,255),(0,255,255)]
-
-  park_data <- readFile "park.svg"
-  let park = SVG.readPolygons park_data
-
-  let cnts = PG.centers park people
-
+  let people  = read people_text
+  let park    = SVG.readPolygons park_data
+  let cnts    = PG.centers park people
   let sitting = Park.sittingNeighbors 4 cnts
   let walking = Park.walkingNeighbors 4 cnts
 
-  let starting_placement = zip cnts people
+  let startingPlacement = zip cnts people
 
-  -- TODO put back to 500
-  let annealing_time = 2
-
-  putStr "starting energy: "
-  print $ Park.picnicEnergy sitting starting_placement
-
-  putStr "starting temperature: "
-  print $ Park.picnicTemperature annealing_time annealing_time
-
-  random_generator <- getStdGen
-  putStr "starting annealing... "
-  putStr "number of annealing steps: "
-  print annealing_time
+  putStrLn $ "Number of people coming: " ++ show (length people)
+  putStrLn $ "number of annealing steps: " ++ show annealTime
+  putStrLn $ "starting energy: "         ++ show (Park.picnicEnergy sitting startingPlacement)
+  putStrLn $ "starting temperature: "    ++ show (Park.picnicTemperature annealTime annealTime)
 
   let aeEnv = SA.AnnealEnv (Park.picnicEnergy sitting)
                            Park.picnicTemperature
                            Park.picnicTransitionalProbability
                            (Park.picnicMotion walking)
 
-  let ideal_placement = SA.anneal aeEnv
-                        annealing_time
-                        random_generator
-                        starting_placement
+  randomGen <- getStdGen
 
-  writeFile "tut9.svg" $ SVG.writePolygons $ map (Park.similarityLine ideal_placement) sitting
-  putStr "Done!\nfinal energy: "
-  print $ Park.picnicEnergy sitting ideal_placement
-  putStr "final temperature: "
-  print $ Park.picnicTemperature 0 annealing_time
+  let finalPlacement = SA.anneal
+                         aeEnv
+                         annealTime
+                         randomGen
+                         startingPlacement
+
+  writeFile "final.svg" $ SVG.writePolygons $ map (Park.similarityLine finalPlacement) sitting
+
+  putStrLn "Done!"
+  putStrLn $ "final energy: " ++ show (Park.picnicEnergy sitting finalPlacement)
+  putStrLn $ "final temperature: " ++ show (Park.picnicTemperature 0 annealTime)
--- haskell/SimulatedAnnealing.hs
+++ haskell/SimulatedAnnealing.hs
@@ -11,25 +11,28 @@ type EnergyFunction a              = a -> Int
 type TemperatureFunction           = Int -> Int -> Float
 type TransitionProbabilityFunction = Int -> Int -> Float -> Float
 type MotionFunction a              = StdGen -> a -> (StdGen,a)
+
+type TimeAllowed = Int
+
 data AnnealEnv a = AnnealEnv {
-  aeEnergy :: EnergyFunction a
-, aeTemperature :: TemperatureFunction
-, aeTransitionProbability :: TransitionProbabilityFunction
-, aeMotion :: MotionFunction a
+  aeEnergy                  :: EnergyFunction a
+, aeTemperature             :: TemperatureFunction
+, aeTransitionProbability   :: TransitionProbabilityFunction
+, aeMotion                  :: MotionFunction a
 }
 
 
-anneal_tick :: MotionFunction a -> TransitionProbabilityFunction -> EnergyFunction a -> Float -> (StdGen,a) -> (StdGen,a)
-anneal_tick mf tpf ef t (r,p) = let (r2,p2) = mf r p
-                                    (n ,r3) = random r2
+annealTick :: MotionFunction a -> TransitionProbabilityFunction -> EnergyFunction a -> Float -> (StdGen,a) -> (StdGen,a)
+annealTick mf tpf ef t (r,p) = let (r2,p2) = mf r p
+                                   (n ,r3) = random r2 :: (Float, StdGen)
                                 in (r3,
                                     if n < tpf (ef p) (ef p2) t
                                     then p2
                                     else p)
 
 anneal' :: EnergyFunction a -> MotionFunction a -> TransitionProbabilityFunction -> TemperatureFunction -> Int -> StdGen -> a -> a
-anneal' ef mf tpf tf m r s = snd $ foldl' (flip (anneal_tick mf tpf ef)) (r,s) (map (tf m) [0..m])
+anneal' ef mf tpf tf m r s = snd $ foldl' (flip (annealTick mf tpf ef)) (r,s) (map (tf m) [0..m])
 
 -- implied extra params time allowed, randGen, starting placement
-anneal :: AnnealEnv annealable -> Int -> StdGen -> annealable -> annealable
+anneal :: AnnealEnv annealable -> TimeAllowed -> StdGen -> annealable -> annealable
 anneal env = anneal' (aeEnergy env) (aeMotion env) (aeTransitionProbability env) (aeTemperature env)
